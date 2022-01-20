0b8f782c7ccb969649418fdc74b125856dec5149
Arif Eker <eng.arifeker@gmail.com>
2022-01-20 14:48:10 +0300

    sys module examples

---
+++ lecture_2/modules/sys_module/sys_module.py
@@ -0,0 +1,29 @@
+#
+#
+# topic numbers = [1, 2, 3]
+# CTRL + F and enter --topic number-- for jump topic to topic
+# for example "--5--"
+
+# topics list:
+
+
+import sys
+
+#########################################################################################################################################################
+# Topic: --1--
+
+print(sys.argv)
+# ['C:\\Program Files\\JetBrains\\PyCharm 2020.2.1\\plugins\\python\\helpers\\pydev\\pydevconsole.py', '--mode=client', '--port=50866']
+
+# open cmd and run sys_module.p and give 2 arguments
+
+print(sys.argv[0])
+print(sys.argv[1])
+print(sys.argv[2])
+
+# sys_module.py
+# argument_1
+# argument_2
+
+
+sys.winver  # 3.7 --> python version number
